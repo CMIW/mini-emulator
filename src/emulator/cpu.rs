191e6357133596081928850243f974f16f1cace2
Aarón Piñar Mora <125222286+16pixar@users.noreply.github.com>
2024-10-29 11:59:16 -0600

    stats changes

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -1,4 +1,5 @@
 use crate::emulator::Operation;
+use std::time::{Instant, Duration};
 
 #[derive(Debug, Default, Copy, Clone)]
 pub struct CPU {
@@ -12,6 +13,7 @@ pub struct CPU {
     pub ir: Option<Operation>,
     pub z: bool,
     pub start_time: Option<std::time::Instant>,
+    pub total_time: Option<Duration>,
 }
 
 impl CPU {
@@ -31,5 +33,30 @@ impl CPU {
             && self.sp == 0
             && self.ir.is_none()
             && !self.z
+    }
+   
+    // Método para iniciar la ejecución de un proceso
+    pub fn start_process(&mut self) {
+        if self.start_time.is_none() {
+            self.start_time = Some(Instant::now());
+            println!("Iniciando proceso en CPU. Tiempo de inicio: {:?}", self.start_time);
+        } else {
+            println!("El proceso ya ha sido iniciado en CPU. Tiempo de inicio existente: {:?}", self.start_time);
+        }
+    }
+
+    // Método para finalizar el proceso y calcular el tiempo de ejecución
+    pub fn finalize_process(&mut self) {
+        if let Some(start) = self.start_time {
+            self.total_time = Some(start.elapsed());
+            println!("Proceso finalizado en CPU. Tiempo total de ejecución: {:?}", self.total_time);
+        
+        }
+        self.clear(); // Limpia el CPU después de finalizar el proceso
+    }
+
+    // Método para limpiar el CPU
+    pub fn clear(&mut self) {
+        *self = CPU::new();
     }
 }
